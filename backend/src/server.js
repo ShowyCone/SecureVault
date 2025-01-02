f6d970e0521c31f62241faaa50e4640c77afb9ab
ShowyCone <alexrafael2003@gmail.com>
2025-01-02 16:51:10 -0400

    🔧 Config: enabled CORS for handling requests between frontend and backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/auth.js'
 import folderRoutes from './routes/folders.js'
 import messageRoutes from './routes/messages.js'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/auth', authRoutes)
 app.use('/folders', folderRoutes)
